197bdc95bc47f30a2e9927399b94853952ff221d
EmTee <mtran0011@outlook.com>
2025-11-04 09:23:34 +1100

    Improved performance by excluding folders

--- dot_ttyd/scripts/visual-studio-launcher.ps1
+++ dot_ttyd/scripts/visual-studio-launcher.ps1
@@ -1,1 +1,1 @@
-(es -r !"*Recycle.Bin*\*" !"*RECYCLE*\*" !"C:\Program*\*" *.sln | fzf --multi --header='Search - .NET Solutions (Tab to Select)') | Where-Object { Start-Process devenv -Argument """$_""" }
+es /a-d -r !"*Recycle.Bin*\*" !"*RECYCLE*\*" !"C:\Program*\*" *.sln | fzf --multi --header='Search - .NET Solutions (Tab to Select)' | Where-Object { Start-Process devenv -Argument """$_""" }
