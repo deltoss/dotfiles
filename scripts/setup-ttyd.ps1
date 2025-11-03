1bb945c13645a4d33d2baf336b8fb37761cc5576
EmTee <mtran0011@outlook.com>
2025-11-04 08:48:21 +1100

    Adjusted ttyd to use -NoProfile for faster launching

--- scripts/setup-ttyd.ps1
+++ scripts/setup-ttyd.ps1
@@ -1,9 +1,9 @@
-$scriptPath = "$env:USERPROFILE/.ttyd/scripts/visual-studio-launcher.ps1"
-$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "-WindowStyle Hidden -Command `"ttyd -p 4090 -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -File `"`"$scriptPath`"`" `""
-# To debug above action, uncomment and use below line:
-# Start-Process -FilePath $action.Execute -ArgumentList $action.Arguments
-
-$taskName = "ttyd - Visual Studio Launcher"
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -Hidden
+
+$scriptPath = "$env:USERPROFILE/.ttyd/scripts/visual-studio-launcher.ps1"
+$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "-WindowStyle Hidden -Command `"ttyd -p 4090 -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File `"`"$scriptPath`"`" `""
+# To debug above action, uncomment and use below line:
+# Start-Process -FilePath $action.Execute -ArgumentList $action.Arguments
+$taskName = "ttyd - Visual Studio Launcher"
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force
