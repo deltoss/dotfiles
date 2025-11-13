60604da8d907d62507f7b3a3ba290496da49c7a0
m.tran <m.tran@leapdev.io>
2025-11-13 17:20:21 +1100

    Improved search to be regex + full path search

--- dot_ttyd/scripts/editor-launcher.ps1
+++ dot_ttyd/scripts/editor-launcher.ps1
@@ -1,4 +1,4 @@
-$esTemplate = "es -sort date-modified-descending count:100 -r !'*Recycle.Bin*\*' !'*RECYCLE*\*' -r {q:1} -r {q:2} -r {q:3} -r {q:4} -r {q:5} -r {q:6} -r {q:7} -r {q:8} -r {q:9}"
+$esTemplate = "es -sort date-modified-descending count:100 -p -r !'\\\`$Recycle.Bin' -r !'^C:\\Program' -r !'C:\\Windows' -r {q:1} -r {q:2} -r {q:3} -r {q:4} -r {q:5} -r {q:6} -r {q:7} -r {q:8} -r {q:9}"
 
 # Pipe null to disable the initial unnecessary search upon entering fzf
 # Sleep command is there to debounce the query so we don't search on every single letter typed
