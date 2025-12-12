2201f4809e256e147642a3efa9285590619d4197
EmTee <>
2025-12-12 17:02:26 +1100

    Updated ttyd scripts to be headless

--- scripts/setup-ttyd.ps1
+++ scripts/setup-ttyd.ps1
@@ -3,21 +3,21 @@ $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-T
 $port = 4090
 
 $scriptPath = "$env:USERPROFILE/.ttyd/scripts/visual-studio-launcher.ps1"
-$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "-WindowStyle Hidden -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File `"`"$scriptPath`"`" `""
+$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File '$scriptPath'`""
 $port++
-# To debug above action, uncomment and use below line:
-# Start-Process -FilePath $action.Execute -ArgumentList $action.Arguments
 $taskName = "ttyd - Visual Studio Launcher"
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
 $scriptPath = "$env:USERPROFILE/.ttyd/scripts/terminal-launcher.ps1"
-$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "-WindowStyle Hidden -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File `"`"$scriptPath`"`" `""
+$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File '$scriptPath'`""
 $port++
 $taskName = "ttyd - Terminal Launcher"
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
 $scriptPath = "$env:USERPROFILE/.ttyd/scripts/editor-launcher.ps1"
-$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "-WindowStyle Hidden -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File `"`"$scriptPath`"`" `""
+$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"ttyd -p $port -t fontSize=20 -t disableLeaveAlert=true --writable -w . pwsh -NoProfile -File '$scriptPath'`""
 $port++
 $taskName = "ttyd - Editor Launcher"
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
+
+Write-Host "Done. ttyd services will start at next login." -ForegroundColor Green
