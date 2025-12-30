67191e81d2720a45298d2f00f78aee3b0d24fa27
EmTee <>
2025-12-31 08:24:17 +1100

    Vastly improved rclone performance

--- scripts/setup-rclone.ps1
+++ scripts/setup-rclone.ps1
@@ -3,11 +3,12 @@ $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-T
 
 if ($env:CHEZMOI_COMPUTERPURPOSE -eq "personal")
 {
-  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"rclone mount --no-console --no-check-certificate --vfs-cache-mode writes --dir-cache-time 5s media-dav: W:`""
+  # --no-check-certificate as we're using http to our local servers
+  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"rclone mount --no-check-certificate --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 128M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M media-dav: W:`""
   $taskName = "RClone - Media"
   Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
-  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"rclone mount personal-google-drive: R:`""
+  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M personal-google-drive: R:`""
   $taskName = "RClone - Personal Google Drive"
   Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
