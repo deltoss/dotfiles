6b60d2aa862a75f338a2c64fb9029f545f4ddedd
emtee <mtran0011@outlook.com>
2026-04-02 16:49:58 +1100

    Updated setup-rclone.ps1 script

--- scripts/PowerShell/setup-rclone.ps1
+++ scripts/PowerShell/setup-rclone.ps1
@@ -1,8 +1,8 @@
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-$settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -Hidden
-
 if ($env:CHEZMOI_COMPUTERPURPOSE -eq "personal")
 {
+  $trigger = New-ScheduledTaskTrigger -AtLogOn
+  $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -Hidden
+
   # --no-check-certificate as we're using http to our local servers
   $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless pwsh -Command `"rclone mount --no-check-certificate --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 128M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M media-dav: W:`""
   $taskName = "RClone - Media"
@@ -12,9 +12,9 @@ if ($env:CHEZMOI_COMPUTERPURPOSE -eq "personal")
   $taskName = "RClone - Personal Google Drive"
   Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
+  Write-Host "Updating sensitive values" -ForegroundColor Green
   rclone config update media-dav pass (op read "op://Personal/Copyparty/password") # Updates the password. Note it's encrypted
   rclone config update personal-google-drive # Updates the access token
-  Write-Host "Updating sensitive values" -ForegroundColor Green
-}
 
-Write-Host "Done. RClone services will start at next login." -ForegroundColor Green
+  Write-Host "Done. RClone services will start at next login." -ForegroundColor Green
+}
