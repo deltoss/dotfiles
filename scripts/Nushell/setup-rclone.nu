e15a8ff30ab9b3ca0992fd95f572dcee22366d66
deltoss <m.tran@leapdev.io>
2026-05-14 13:47:35 +1000

    Fixed rclone setup + added support for notes-google-drive

--- scripts/Nushell/setup-rclone.nu
+++ scripts/Nushell/setup-rclone.nu
@@ -1,18 +1,24 @@
 #!/usr/bin/env nu
 
-if ($env.CHEZMOI_COMPUTERPURPOSE == "personal") {
-  print $"(ansi green)Configuring rclone...(ansi reset)"
+print $"(ansi green)Configuring rclone...(ansi reset)"
 
+let is_config_setup = rclone config redacted | lines | any { $in =~ notes-google-drive }
+if (not $is_config_setup) {
+  print $"(ansi green)Updating sensitive values...(ansi reset)"
+  rclone config update notes-google-drive # Updates the access token
+}
+
+if ($env.CHEZMOI_COMPUTERPURPOSE == "personal") {
   let is_config_setup = rclone config redacted | lines | any { $in =~ media-dav }
   if (not $is_config_setup) {
-    print $"(ansi green)Updating sensitive values...(ansi reset)"
+    print $"(ansi green)Updating sensitive values specifically for personal computer...(ansi reset)"
     rclone config update media-dav pass (op read "op://Personal/Copyparty/password") # Updates the password. Note it's encrypted
     rclone config update personal-google-drive # Updates the access token
   }
-
-  if (sys host).name =~ "Windows" {
-    print $"(ansi green)Configuring startup for Windows...(ansi reset)"
-    ^powershell -ExecutionPolicy Bypass -File $'($env.CHEZMOI_SOURCE_DIR)/scripts/PowerShell/setup-rclone-tasks.ps1'
-  }
-  print $"(ansi green)Done. rclone services will start at next login.(ansi reset)"
 }
+
+if (sys host).name =~ "Windows" {
+  print $"(ansi green)Configuring startup for Windows...(ansi reset)"
+  ^powershell -ExecutionPolicy Bypass -File $'($env.CHEZMOI_SOURCEDIR)/scripts/PowerShell/setup-rclone-tasks.ps1'
+}
+print $"(ansi green)Done. rclone services will start at next login.(ansi reset)"
--- scripts/PowerShell/setup-rclone-tasks.ps1
+++ scripts/PowerShell/setup-rclone-tasks.ps1
@@ -1,11 +1,17 @@
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -Hidden
 
-# --no-check-certificate as we're using http to our local servers
-$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --no-check-certificate --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 128M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M media-dav: W:`""
-$taskName = "RClone - Media"
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
+if ($env:CHEZMOI_COMPUTERPURPOSE -eq "personal") {
+  # --no-check-certificate as we're using http to our local servers
+  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --no-check-certificate --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 128M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M media-dav: W:`""
+  $taskName = "RClone - Media"
+  Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
-$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M personal-google-drive: R:`""
-$taskName = "RClone - Personal Google Drive"
+  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M personal-google-drive: R:`""
+  $taskName = "RClone - Personal Google Drive"
+  Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
+}
+
+$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M notes-google-drive: N:`""
+$taskName = "RClone - Notes Google Drive"
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
