ac0806fc6d74b35364dcd21e92be91770a4d718a
deltoss <m.tran@leapdev.io>
2026-05-14 16:50:29 +1000

    Adjusted rclone tasks

--- scripts/PowerShell/setup-rclone-tasks.ps1
+++ scripts/PowerShell/setup-rclone-tasks.ps1
@@ -4,14 +4,22 @@ $settings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-T
 if ($env:CHEZMOI_COMPUTERPURPOSE -eq "personal") {
   # --no-check-certificate as we're using http to our local servers
   $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --no-check-certificate --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 128M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M media-dav: W:`""
-  $taskName = "RClone - Media"
+  $taskName = "RClone - Media - WebDav"
   Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 
   $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M personal-google-drive: R:`""
-  $taskName = "RClone - Personal Google Drive"
+  $taskName = "RClone - Mount Personal Drive - Google Drive"
+  Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
+
+  $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M personal-google-drive: '$env:USERPROFILE\Documents\Personal'`""
+  $taskName = "RClone - Mount Personal Folder - Google Drive"
   Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
 }
 
 $action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M notes-google-drive: N:`""
-$taskName = "RClone - Notes Google Drive"
+$taskName = "RClone - Mount Notes Drive - Google Drive"
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
+
+$action = New-ScheduledTaskAction -Execute "conhost.exe" -Argument "--headless powershell -Command `"rclone mount --vfs-cache-mode full --vfs-cache-max-size 20G --buffer-size 32M --dir-cache-time 1h --poll-interval 30s --vfs-read-chunk-size 32M notes-google-drive:Notes '$env:USERPROFILE\Documents\Synced Notes'`""
+$taskName = "RClone - Mount Notes Folder - Google Drive"
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force | Out-Null
